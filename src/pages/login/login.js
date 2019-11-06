decd23f046970b3ffa96d2d21376d41896f2b593
lihanyang <18515605253@163.com>
2019-11-06 10:13:51 +0800

    ceshi

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import './login.less'
 class Login extends Component {
-    render() { 
-        return ( 
-            <div>
-                Login
+    render() {
+        return (
+            <div className="login">
+                <div className="login-header">
+                    <img src="" alt=""/>
+                    <h1>欢网 . 后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h1>用户登录</h1>
+                    <div>from表单</div>
+                </div>
+
             </div>
         );
     }
 }
- 
+
 export default Login;
