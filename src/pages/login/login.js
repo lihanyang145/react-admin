f57735545f16256fba14877a72ca54f0018761b4
lihanyang <18515605253@163.com>
2019-11-06 14:12:17 +0800

    login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import './login.less'
-
+import logo from './images/logo.png'
+import { Form, Icon, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
 class Login extends Component {
-    render() { 
-        return ( 
-           <div className="login">1111111111</div>
-         );
+    render() {
+        return (
+            <div className="login">
+                <div className="login-header">
+                    <img src={logo} alt="logo" />
+                    <h1>欢网.后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h1>用户登录</h1>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="用户名"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary"  htmlType="submit" className="login-form-button">登录</Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        );
+
     }
+    handleSubmit = e => {
+        e.preventDefault();
+        alert("提交")
+    };
 }
- 
+
 export default Login;
