169901da1c3fb7efc9f217a8a50e87c360d7844e
lihanyang <18515605253@163.com>
2019-11-08 10:50:09 +0800

    left-nav

--- src/components/left-nav/left-nav.js
+++ src/components/left-nav/left-nav.js
@@ -1,12 +1,78 @@
 import React, { Component } from 'react';
 import './left-nav.less';
+import { Link, withRouter } from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import { Menu, Icon } from 'antd';
+import menulist from '../../config/menuConfig'
+const { SubMenu } = Menu;
 class LeftNav extends Component {
+    componentWillMount() {
+       this.menuNodes = this.getMenusNodes(menulist)
+    }
 
-    render() { 
-        return ( 
-            <div className='left-nav'>left-nav</div>
-         );
+    render() {
+        //获取当前的路由地址
+        const seleteKey = this.props.location.pathname
+      
+        return (
+            <div className='left-nav'>
+                <Link className="left-nav-link" to="/home">
+                    <img src={logo} alt="logo" />
+                    <h1>欢网科技</h1>
+                </Link>
+                <Menu
+                    selectedKeys={[seleteKey]}
+                    defaultOpenKeys={[this.openKey]}
+                    mode="inline"
+                    theme="dark"
+                >
+                    {this.menuNodes}
+
+                </Menu>
+            </div>
+        );
+    }
+    //map+递归函数实现
+    getMenusNodes = (menulist) => {
+        const path = this.props.location.pathname
+        return menulist.map(item => {
+            if (!item.children) {
+                return (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon} />
+                            <span>{item.title} </span>
+                        </Link>
+                    </Menu.Item>
+                )
+            } else {
+                const cItem = item.children.find(cItem => cItem.key === path)
+                if (cItem) {
+                    this.openKey = item.key
+                }
+
+
+                return (
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                        }>
+                        {
+                            this.getMenusNodes(item.children)
+                        }
+                    </SubMenu>
+                )
+
+            }
+
+
+        })
+
     }
 }
- 
-export default LeftNav;
+//使用高阶组件包装非路由组件
+export default withRouter(LeftNav);
--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import storageUtil from '../../utils/storageUtils'
-import { Redirect ,Switch ,Route } from 'react-router-dom'
+import { Redirect, Switch, Route } from 'react-router-dom'
 import { Layout } from 'antd';
 import LeftNav from '../../components/left-nav/left-nav';
 import Header from '../../components/header/header';
 import Home from '../home/home';
 import Product from '../product/product';
+import Cotegory from '../cotegory/cotegory';
 import Role from '../role/role';
 import User from '../user/user';
 import Line from '../charts/line/line';
 import Bar from '../charts/bar/bar';
 import Pie from '../charts/pie/pie';
-const {  Footer, Sider, Content } = Layout;
+const { Footer, Sider, Content } = Layout;
 class Admin extends Component {
     render() {
         const username = storageUtil.getUser();
-        console.log(username)
         if (!username) {
             return <Redirect to="/login" />
         }
         return (
-            <Layout style={{height:'100%'}}>
+            <Layout style={{ height: '100%' }}>
                 <Sider>
-                    <LeftNav/>
+                    <LeftNav />
                 </Sider>
                 <Layout>
-                    <Header/>
-                    <Content style={{backgroundColor:'#454554'}}>
+                    <Header />
+                    <Content style={{ backgroundColor: '#454554' }}>
                         <Switch>
-                            <Route path="/home" component={Home}/>
-                            <Route path="/product" component={Product}/>
-                            <Route path="/role" component={Role}/>
-                            <Route path="/user" component={User}/>
-                            <Route path="/charts/line" component={Line}/>
-                            <Route path="/charts/bar" component={Bar}/>
-                            <Route path="/charts/pie" component={Pie}/>
+                            <Route path="/home" component={Home} />
+                            <Route path="/product" component={Product} />
+                            <Route path="/cotegory" component={Cotegory} />
+                            <Route path="/role" component={Role} />
+                            <Route path="/user" component={User} />
+                            <Route path="/charts/line" component={Line} />
+                            <Route path="/charts/bar" component={Bar} />
+                            <Route path="/charts/pie" component={Pie} />
                             <Redirect to="/home"/>
                         </Switch>
-                        
+
                     </Content>
-                    <Footer style={{textAlign:'center'}}>
+                    <Footer style={{ textAlign: 'center' }}>
                         推荐使用谷歌浏览器,可以获得更佳页面操作体验
                     </Footer>
                 </Layout>
